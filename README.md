4b80dcee72fd180c9366e94536a4e0c24ac433c8
mymi14s <mymi14s@hotmail.com>
2023-08-14 11:44:12 +0100

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Learning Management by ONE FM
+
+Extention to Frappe LMS
+
+#### License
+
+MIT
---
+++ one_lms/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Learning Management by ONE FM",
+			"type": "module",
+			"label": _("Learning Management by ONE FM")
+		}
+	]
