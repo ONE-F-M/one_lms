384731c37d4b385b56c3f4dedc0f02dc5a116602
Anthony C. Emmanuel <mymi14s@hotmail.com>
2023-11-08 06:20:30 +0100

    Update __init__.py

--- one_lms/__init__.py
+++ one_lms/__init__.py
@@ -1,9 +1,10 @@
-__version__ = '14.0.1'
 from lms.lms import  utils
 from one_lms.overrides.utils import get_lesson_details as custom_gld,set_template_path_
 from lms.www.batch import learn
 from frappe.website.page_renderers.template_page import TemplatePage
 from one_lms.www.batch.learn import get_context
+
+__version__ = '14.0.1'
 
 utils.get_lesson_details = custom_gld
 TemplatePage.set_template_path =  set_template_path_
