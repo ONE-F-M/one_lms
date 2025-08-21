18ecfc7664063d817c794492d22a21fa8092beb4
Jamsheer PP <kuttypjamsheer@gmail.com>
2025-08-21 06:39:17 +0530

    feat: Shuffling Quiz Questions

--- one_lms/__init__.py
+++ one_lms/__init__.py
@@ -1,4 +1,4 @@
-from one_lms.overrides.plugins import assignment_renderer
+from one_lms.overrides.plugins import assignment_renderer, quiz_renderer
 from lms import plugins
 import lms.utils
 import one_lms.utils
@@ -6,4 +6,5 @@ import one_lms.utils
 __version__ = "0.0.1"
 
 plugins.assignment_renderer = assignment_renderer
+plugins.quiz_renderer = quiz_renderer
 lms.utils.create_notification_log = one_lms.utils.create_notification_log
--- one_lms/overrides/plugins.py
+++ one_lms/overrides/plugins.py
@@ -1,3 +1,4 @@
+import random
 import frappe
 from urllib.parse import quote
 
@@ -19,3 +20,73 @@ def assignment_renderer(detail):
             "accept": accept,
         },
     )
+
+def quiz_renderer(quiz_name):
+	if frappe.session.user == "Guest":
+		return " <div class='alert alert-info'>" + _(
+			"Quiz is not available to Guest users. Please login to continue."
+		)
+		+"</div>"
+
+	quiz = frappe.db.get_value(
+		"LMS Quiz",
+		quiz_name,
+		[
+			"name",
+			"title",
+			"max_attempts",
+			"show_answers",
+			"show_submission_history",
+			"passing_percentage",
+		],
+		as_dict=True,
+	)
+	quiz.questions = []
+	fields = ["name", "question", "type", "multiple"]
+	for num in range(1, 5):
+		fields.append(f"option_{num}")
+		fields.append(f"is_correct_{num}")
+		fields.append(f"explanation_{num}")
+		fields.append(f"possibility_{num}")
+
+	questions = frappe.get_all(
+		"LMS Quiz Question",
+		filters={"parent": quiz.name},
+		fields=["question", "marks"],
+		order_by="idx",
+	)
+
+	question_list = []
+	for question in questions:
+		details = frappe.db.get_value("LMS Question", question.question, fields, as_dict=1)
+		details["marks"] = question.marks
+		question_list.append(details)
+
+	random.shuffle(question_list)
+
+	quiz.questions = question_list
+
+	no_of_attempts = frappe.db.count(
+		"LMS Quiz Submission", {"owner": frappe.session.user, "quiz": quiz_name}
+	)
+
+	if quiz.show_submission_history:
+		all_submissions = frappe.get_all(
+			"LMS Quiz Submission",
+			{
+				"quiz": quiz.name,
+				"member": frappe.session.user,
+			},
+			["name", "score", "creation"],
+			order_by="creation desc",
+		)
+
+	return frappe.render_template(
+		"templates/quiz/quiz.html",
+		{
+			"quiz": quiz,
+			"no_of_attempts": no_of_attempts,
+			"all_submissions": all_submissions if quiz.show_submission_history else None,
+			"hide_quiz": False,
+		},
+	)
