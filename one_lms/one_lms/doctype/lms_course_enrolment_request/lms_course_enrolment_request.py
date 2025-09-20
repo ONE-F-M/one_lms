46fc59fb2de745f0f37b033d07765e936a40147f
jam <jam@jams-MacBook-Air.local>
2025-09-20 17:05:25 +0530

    feat: CER approval notification

--- one_lms/one_lms/doctype/lms_course_enrolment_request/lms_course_enrolment_request.py
+++ one_lms/one_lms/doctype/lms_course_enrolment_request/lms_course_enrolment_request.py
@@ -10,11 +10,9 @@ class LMSCourseEnrolmentRequest(Document):
 
     def notify_instructors_on_request(self):
         try:
-            course_doc = frappe.get_doc("LMS Course", self.course)
-            instructor_emails = self.get_instructors()
-            if instructor_emails:
-                member_doc = frappe.get_doc("User", self.member)
-                content = self.get_email_content(member_doc)
+            instructors = self.get_instructors()
+            if instructors:
+                content = self.get_email_content_for_instructor()
                 context = dict(
                     header="Dear Instructor,<br/> Good day.",
                     document_name=self.name,
@@ -24,44 +22,44 @@ class LMSCourseEnrolmentRequest(Document):
                     content=content,
                     link_name="Link to the Course Enrolment Request",
                 )
-                subject = f"Course Enrollment Request: {self.course}"
+                subject = f"Course Enrollment Request: {self.course_title or self.course}"
                 
                 msg = frappe.render_template('one_lms/templates/emails/default_email.html', context=context)
                 frappe.sendmail(
-                    recipients=instructor_emails,
+                    recipients=instructors,
                     subject=subject,
                     message=msg
                 )
                 # Create Notification Log for each instructor
-                self.create_notification_log(instructor_emails, subject, msg)
+                self.create_notification_log(instructors, subject, msg)
         except Exception as e:
             frappe.log_error(message=f"Error sending instructor notification: {e}", title="LMS Enrolment Request Notification")
 
     def get_instructors(self):
         course_doc = frappe.get_doc("LMS Course", self.course)
-        instructor_emails = []
+        instructors = []
         if course_doc.instructors:
             for instructor in course_doc.instructors:
                 if instructor.instructor:
-                    instructor_emails.append(instructor.instructor)
-        return instructor_emails
+                    instructors.append(instructor.instructor)
+        return instructors
 
-    def get_email_content(self, member_doc):
+    def get_email_content_for_instructor(self):
         return f"""The details of the enrolment request are shown below:
             <br/><br/>
-            Employee ID: {member_doc.username}
+            Employee ID: {self.username}
             <br/>
-            Employee Name: {member_doc.full_name or self.member}
+            Employee Name: {self.member_name or self.member}
             <br/>
             Email: {self.member}
             <br/>
-            Course Name: {self.course}
+            Course Name: {self.course_title or self.course}
             <br/>
             Request Date: {frappe.utils.format_datetime(self.creation, "medium")}
         """
 
-    def create_notification_log(self, instructor_emails, subject, msg):
-        for user in instructor_emails:
+    def create_notification_log(self, emails, subject, msg):
+        for user in emails:
             notification_doc = frappe.get_doc({
                 "doctype": "Notification Log",
                 "subject": subject,
@@ -84,9 +82,14 @@ class LMSCourseEnrolmentRequest(Document):
         return "success"
 
     def on_update(self):
-        if self.status in ["Approved", "Rejected"]:
-            self.notify_member_on_status_change()
+        if self.status == "Approved":
             self.create_enrollment()
+            self.notify_member_on_instructor_action(
+                description="Your course enrollment request has been approved.",
+                action="Approved",
+                document_link=frappe.utils.get_url(f"lms/courses/{self.course}"),
+                link_name="Link to the Course Page"
+            )
 
     def create_enrollment(self):
         if self.status != "Approved":
@@ -105,50 +108,36 @@ class LMSCourseEnrolmentRequest(Document):
             "member": self.member,
         }).insert(ignore_permissions=True)
 
-    def notify_member_on_status_change(self):
-        try:
-            member_doc = frappe.get_doc("User", self.member)
-            member_name = member_doc.full_name or self.member
-            course_doc = frappe.get_doc("LMS Course", self.course)
-            if self.status == "Approved":
-                subject = f"Course Enrolment Request Approved - {course_doc.title}"
-                message = f"""
-                <p>Dear {member_name},</p>
-                <p>Great news! Your course enrolment request has been <strong>approved</strong>.</p>
-                <ul>
-                <li><strong>Course:</strong> {course_doc.title}</li>
-                <li><strong>Status:</strong> Approved</li>
-                <li><strong>Date:</strong> {frappe.format(frappe.utils.now(), 'datetime')}</li>
-                </ul>
-                <p>You can now access the course content and begin your learning journey.</p>
-                <p>Happy Learning!</p>
-                """
-            else:
-                subject = f"Course Enrolment Request Rejected - {course_doc.title}"
-                message = f"""
-                <p>Dear {member_name},</p>
-                <p>We regret to inform you that your course enrolment request has been <strong>rejected</strong>.</p>
-                <ul>
-                <li><strong>Course:</strong> {course_doc.title}</li>
-                <li><strong>Status:</strong> Rejected</li>
-                <li><strong>Date:</strong> {frappe.format(frappe.utils.now(), 'datetime')}</li>
-                </ul>
-                <p>Please contact the course instructors for more information.</p>
-                <p>Thank you.</p>
-                """
-            notification_doc = frappe.get_doc({
-                "doctype": "Notification Log",
-                "subject": subject,
-                "email_content": message,
-                "document_type": self.doctype,
-                "document_name": self.name,
-                "from_user": frappe.session.user,
-                "type": "Alert",
-                "for_user": self.member
-            })
-            notification_doc.insert(ignore_permissions=True)
-        except Exception as e:
-            frappe.log_error(f"Error sending member notification", "LMS Enrolment Request Status Notification")
+    def notify_member_on_instructor_action(self, description, action, document_link, link_name):
+        content = self.get_email_content_for_member()
+        context = dict(
+            header="Dear {0},<br/> Good day.".format(self.member_name or self.member),
+            document_name=self.name,
+            document_type=self.doctype,
+            document_link=document_link,
+            description=description,
+            content=content,
+            link_name=link_name,
+        )
+        subject = f"Course Enrollment Request for {self.course_title} has been {action}"
+
+        msg = frappe.render_template('one_lms/templates/emails/default_email.html', context=context)
+        frappe.sendmail(
+            recipients=[self.member],
+            subject=subject,
+            message=msg
+        )
+        # Create Notification Log for each instructor
+        self.create_notification_log([self.member], subject, msg)
+
+    def get_email_content_for_member(self):
+        return f"""
+            Course Name: {self.course_title or self.course}
+            <br/>
+            Approver: {frappe.db.get_value("User", frappe.session.user, "full_name") or frappe.session.user}
+            <br/>
+            Decision Date: {frappe.utils.format_datetime(self.modified, "medium")}
+        """
 
 @frappe.whitelist()
 def create_lms_course_enrolment_request(course, member=None):
